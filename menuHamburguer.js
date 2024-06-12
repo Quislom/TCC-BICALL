ba507316ea6deed49afe94a469dee9a94cd9d4c9
Quislom <147166703+Quislom@users.noreply.github.com>
2024-06-12 09:08:13 -0300

    dark-mode
    Adicionando o modo dark (modo batman)

--- menuHamburguer.js
+++ menuHamburguer.js
@@ -3,3 +3,24 @@ let div = document.querySelector(".sidebar");
 btn.addEventListener("click", ()=>{
   div.classList.toggle("active")
 })
+
+
+document.getElementById('toggle-dark-mode').addEventListener('click', function(){ 
+  document.body.classList.toggle('dark-mode');
+
+  const blocoAmareloLinks = document.querySelector('.blocoAmareloLinks');
+  if (blocoAmareloLinks){
+    blocoAmareloLinks.classList.toggle('dark-mode');
+  }
+
+  const fundo = document.querySelector('.fundo'); 
+  if (fundo){
+    fundo.classList.toggle('dark-mode');
+  }
+
+  const txtBannerElements = document.querySelectorAll('.txtBanner');
+  txtBannerElements.forEach(function(element){
+    element.classList.toggle('dark-mode');
+  });
+});
+
